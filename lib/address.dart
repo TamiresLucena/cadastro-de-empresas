e3e000a1e8c17a6c43c4d97f5df268a505a10e60
TamiresLucena <tamiresb.lucena@gmail.com>
2022-09-07 13:06:17 -0300

    feat: add class address

---
+++ lib/address.dart
@@ -0,0 +1,11 @@
+class Address {
+  String street;
+  int number;
+  String? complement;
+  String neighborhood;
+  String state;
+  String zipCode;
+
+  Address(this.street, this.number, this.complement, this.neighborhood,
+      this.state, this.zipCode);
+}
