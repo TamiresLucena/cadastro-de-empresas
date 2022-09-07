4d6c938de443e14bfaf9f1c2ae75c6509354e166
TamiresLucena <tamiresb.lucena@gmail.com>
2022-09-07 13:04:39 -0300

    feat: initial config

---
+++ test/cadastro_de_empresas_test.dart
@@ -0,0 +1,8 @@
+// import 'package:cadastro_de_empresas/company.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
